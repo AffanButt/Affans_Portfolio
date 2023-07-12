edb20bc1bfca200bb6418230a213562a80c94116
Affan Butt <buttaffan193@gmail.com>
2023-07-12 20:12:06 +0500

    New Project to push on GitHub

---
+++ src/content_option.js
@@ -0,0 +1,216 @@
+const logotext = "M.Affan";
+const meta = {
+  title: "Affan Butt",
+  description:
+    "I’m M Affan SQA Engineer & Project Manager _ WordPress & React Developer,currently working on Upwork",
+};
+
+const introdata = {
+  title: "I’m M Affan Butt",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third:
+      "I'm a SQA Engineer and Analyst with Strong Project Management Skills'",
+  },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url: "https://images.unsplash.com/flagged/photo-1574885173944-c23db8159846?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
+};
+
+const dataabout = {
+  title: "About Myself",
+  aboutme:
+   "Hi, I am M Affan Butt, an ambitious and self-motivated Project Manager and QA professional with more than 3 years of experience.I'm an ISTQB-certified tester. I have been providing my services to a Canadian Software Company BBITS.SOLUTIONS and delivered them with my remarkable services as an SQA Tester and Engineer.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Project of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Project of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Project of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Selenium Web-Driver",
+    value: 90,
+  },
+  {
+    name: "Project Management",
+    value: 100,
+  },
+  {
+    name: "Functionality Testing",
+    value: 100,
+  },
+  {
+    name: "Front-End React JS",
+    value: 80,
+  },
+  {
+    name: "Website and Mobile Application Testing",
+    value: 100,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "I have a proven track record of success in designing user-centered interfaces that are both effective and aesthetically pleasing. I am passionate about creating products that people love to use.",
+  },
+  {
+    title: "Mobile App Development",
+    description:
+      "I am a mobile app developer with 3 years of experience. I am proficient in Java, Swift, and other programming languages. I have experience developing apps for both Android and iOS platforms.",
+  },
+  {
+    title: "Wordpress Development",
+    description:
+      "I have a proven track record of success in developing high-quality WordPress websites that meet the needs of my clients. I am passionate about WordPress, and I am committed to providing my clients with the best possible service.",
+  },
+  {
+    title: "Selenium Web-Driver",
+    description:
+      "I am passionate about quality assurance, and I am committed to ensuring that the software I test is of the highest quality. I am always looking for ways to improve my testing skills and knowledge, and I am always willing to help others learn and grow.",
+  },
+  {
+    title: "Project Management",
+    description:
+      "I am a GOOGLE CERTIFIED Project Manager with 4 years of experience having strong understanding of Project Management principles and I am proficient in a variety of Project Management tools and I'm able to successfully manage projects of all sizes and complexities.I am skilled in using a variety of project management methodologies, including Waterfall, Agile, and Scrum. I am also familiar with the latest project management technologies. I have a proven track record of success in delivering projects on time, on budget, and to the required quality standards. I am passionate about project management, and I am committed to ensuring that projects are successful.",
+  },
+  
+  {
+    title: "Functionality Testing",
+    description:
+      "I have a proven track record of success in finding and fixing bugs in software applications. I am passionate about quality assurance, and I am committed to ensuring that software is of the highest quality.",
+  },
+  {
+    title: "Front-End Developer React JS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wesbite and Mobile Application Testing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "buttaffan193@gmail.com",
+  YOUR_FONE: "0333-5754446",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_ynnwoik",
+  YOUR_TEMPLATE_ID: "template_ss0xvuf",
+  YOUR_USER_ID: "Mcih_6NLt-AyGx_QS",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
